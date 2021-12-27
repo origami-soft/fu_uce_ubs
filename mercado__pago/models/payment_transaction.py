2dfb00647597f8c55bd30889b540173c9f170c2f
Leandro Ezequiel Baldi <leandro@blueorange.com.ar>
2021-12-27 14:37:53 -0300

    odoo_l10n_ar 13.0

--- mercado__pago/models/payment_transaction.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class payment_transaction(models.Model):
-    _inherit = 'payment.transaction'
-
-    mp_response = fields.Html( string="mercadopago")
-    mp_json_response = fields.Text( string="JSON MP Response")
