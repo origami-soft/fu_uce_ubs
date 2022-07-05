e7cc0b92c81ea7d54efe3f84e2f6594503e57218
Juan Pablo <juan.pablo@octopia.com.ar>
2022-07-05 08:56:46 -0300

    Borrar Modulo whatsapp
    Borrar Modulo

--- whatsapp_connector_mass/models/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from . import mailing_contact
-from . import mailing_list
-from . import mailing_mailing
-from . import mailing_trace
-from . import message
-from . import utm
-from . import ResConfigSettings
-from . import account_move
